7e45b50ad5b1cb337f4b55f9b1407e3f414948b5
Alexis Jesus Buelvas Madera <abuelvas@gattaca.mx>
2025-03-31 08:25:34 -0500

    feat: Implement favorite page

--- src/app/pages/breed-favorite/breed-favorite.component.ts
+++ src/app/pages/breed-favorite/breed-favorite.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import {
+  AfterViewInit,
+  Component,
+  Inject,
+  OnInit,
+  PLATFORM_ID,
+} from '@angular/core';
+import { DogService } from '../../core/services/dog.service';
+import { HeaderComponent } from '../../layouts/header/header.component';
+import { ButtonComponent } from '../../shared/components/button/button.component';
+import { ImageCardComponent } from '../../shared/components/image-card/image-card.component';
 
 @Component({
   selector: 'app-breed-favorite',
-  imports: [],
+  imports: [HeaderComponent, ImageCardComponent, ButtonComponent, CommonModule],
   templateUrl: './breed-favorite.component.html',
-  styleUrl: './breed-favorite.component.scss'
+  styleUrl: './breed-favorite.component.scss',
 })
-export class BreedFavoriteComponent {
+export class BreedFavoriteComponent implements OnInit, AfterViewInit {
+  constructor(
+    private dogService: DogService,
+    @Inject(PLATFORM_ID) private platformId: Object,
+  ) {}
 
+  isClient = false;
+  favorites: string[] = [];
+
+  ngOnInit() {
+    this.isClient = isPlatformBrowser(this.platformId);
+    this.favorites = this.dogService.getFavorites();
+  }
+
+  ngAfterViewInit() {
+    this.isClient = true;
+  }
+
+  removeFavorite(dog: string) {
+    this.dogService.removeFavorite(dog);
+    this.favorites = this.favorites.filter((fav) => fav !== dog);
+  }
 }
