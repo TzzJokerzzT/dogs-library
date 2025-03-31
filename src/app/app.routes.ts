38d26d45ae08ddccbbd9386a4cfd00de9e2922b7
Alexis Jesus Buelvas Madera <abuelvas@gattaca.mx>
2025-03-31 08:27:29 -0500

    Configuration project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { BreedFavoriteComponent } from './pages/breed-favorite/breed-favorite.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'favorite', component: BreedFavoriteComponent },
+];
